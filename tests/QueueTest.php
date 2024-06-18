1bc1a0d7d0dad0fed8fb646e5bea14593af90b21
Tiago Bordin <tiagolemespalhano@gmail.com>
2024-06-18 16:59:55 -0300

    16. Fixtures: set up the known state of the tests using setUp and tearDown

--- tests/QueueTest.php
+++ tests/QueueTest.php
@@ -4,35 +4,42 @@ use PHPUnit\Framework\TestCase;
 
 class QueueTest extends TestCase
 {
+
+	protected $queue;
+
+	protected function setUp() : void
+	{
+		$this->queue = new Queue;
+	}
+
+
+	protected function tearDown() :
+	{
+		unset($this->queue);
+	}
+
+
 	public function testNewQueueIsEmpty()
 	{
-		$queue = new Queue;
-
-		$this->assertEquals(0, $queue->getCount());
-
-		return $queue;
+		$this->assertEquals(0, $this->queue->getCount());		
 	}
 
-	/**
-	 * @depends testNewQueueIsEmpty
-	 */
-	public function testAnItemIsAddedToTheQueue(Queue $queue) 
+	
+	public function testAnItemIsAddedToTheQueue() 
 	{
-		$queue->push('green');
+		$this->queue->push('green');
 
-		$this->assertEquals(1, $queue->getCount());
-
-		return $queue;
+		$this->assertEquals(1, $this->queue->getCount());		
 	}
 
-	/**
-	 * @depends testAnItemIsAddedToTheQueue
-	 */
-	public function testAnItemIsRemovedFromTheQueue(Queue $queue)
+	
+	public function testAnItemIsRemovedFromTheQueue()
 	{	
-		$item = $queue->pop();
+		$this->queue->push('green');
 
-		$this->assertEquals(0, $queue->getCount());
+		$item = $this->queue->pop();
+
+		$this->assertEquals(0, $this->queue->getCount());
 
 		$this->assertEquals('green', $item);
 	}
